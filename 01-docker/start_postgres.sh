141386a3b8cb726b42936aaaee3621f638a86ba9
Nicolas Alonso <nicolasalonso@Nicolass-MacBook-Air.local>
2025-10-13 17:38:16 +0200

    Fix start_up file for dynamic OS volumen path

--- 01-docker/start_postgres.sh
+++ 01-docker/start_postgres.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+HOST_DATA_PATH="./ny_taxi_postgres_data"
+
 podman run -it \
   -e POSTGRES_USER="root" \
   -e POSTGRES_PASSWORD="root" \
   -e POSTGRES_DB="ny_taxi" \
-  -v \c\Users\Z146005\Documents\code\nico\zoo\nic\01-dockerny_taxi_postgres_data:/var/lib/postgresql/data \
+  -v "$HOST_DATA_PATH":/var/lib/postgresql/data \
   -p 5432:5432 \
   postgres:13
